b5d4e61f753d8a33499c6bed2c920c511fb92d9a
dadjin <dadjin16@gmail.com>
2018-11-06 22:16:57 +0700

    Property Binding

--- src/app/components/hello-world/hello-world.component.ts
+++ src/app/components/hello-world/hello-world.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-hello-world',
-  templateUrl: './hello-world.component.html',
-  styleUrls: ['./hello-world.component.css']
+  selector: "app-hello-world",
+  templateUrl: "./hello-world.component.html",
+  styleUrls: ["./hello-world.component.css"]
 })
 export class HelloWorldComponent implements OnInit {
-  public name:string ="Angular";
+  public name: string = "Angular";
+  public status:boolean = false;
+  public imgUrl = "https://angular.io/assets/images/logos/angular/logo-nav@2x.png";
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
