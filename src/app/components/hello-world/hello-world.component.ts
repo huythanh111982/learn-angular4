9dd961fb64e3924a860e4049e2ffecf1d8241d09
dadjin <dadjin16@gmail.com>
2018-11-06 22:00:06 +0700

    String Interpolation

--- src/app/components/hello-world/hello-world.component.ts
+++ src/app/components/hello-world/hello-world.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello-world.component.css']
 })
 export class HelloWorldComponent implements OnInit {
-  public name:string ="Angular 4";
+  public name:string ="Angular";
 
   constructor() { }
 
